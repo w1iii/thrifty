20605720801cb3fa41738e91abe97675aab38e51
wii <lomugdanglf.19@gmail.com>
2025-12-07 16:02:35 +0800

    Landing page on progress

---
+++ src/components/Landingpage.jsx
@@ -0,0 +1,20 @@
+import Navbar from './Navbar.jsx'
+import './Landingpage.css'
+
+function Landingpage(){
+
+  return(
+    <>
+      <Navbar />
+
+      <div className="main-container">
+        <h1>Thrifty</h1>
+        <h2>Swipe. Save. Discover thrift gems.</h2>
+        <p>Thrifty turns thrifting into a simple swipe experience. Browse a personalized feed of secondhand gems, swipe right to save, left to skip, and discover amazing deals—all in one smooth.</p>
+      </div>
+    </>
+
+  )
+}
+
+export default Landingpage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./Navbar.css"
+
+
+function Navbar(){
+  return(
+    <>
+      <div className="navbar-container">
+        <div className="navlogo">
+          <h1> Thrifty </h1>
+        </div>
+        <div className="navbuttons">
+
+          <a href="" target="_self">Login</a>
+          <a href="" target="_self">Sign up</a>
+        </div>
+      </div>
+    </>
+  )
+
+}
+
+
+export default Navbar
