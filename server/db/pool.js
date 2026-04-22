4d75487754bdca97d82bbe7a591c3eada836daef
wii <lomugdanglf.19@gmail.com>
2026-04-22 22:27:52 +0800

    Update pool.js for production deployment

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,11 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'wii',
-  password: '101904',
-  host: 'localhost',
-  port: 5432,
-  database: 'thrifty'
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
