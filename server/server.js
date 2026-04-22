9dcf2034def7755585e2105190a147dea5e3038a
wii <lomugdanglf.19@gmail.com>
2026-04-22 22:59:53 +0800

    Add Vercel frontend to CORS

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173', 'https://thrifty-bice.vercel.app'],
   credentials: true, // Allow credentials
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
