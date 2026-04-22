99fc8f64eac87f83808727e583d32e79494229d5
wii <lomugdanglf.19@gmail.com>
2026-04-22 23:42:42 +0800

    Debug DATABASE_URL

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,8 +1,11 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL ? 'exists' : 'MISSING!');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
