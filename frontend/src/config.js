4765afa3233a4b721a8538c5cd80fca59fbfbc53
wii <lomugdanglf.19@gmail.com>
2026-04-23 11:45:46 +0800

    updated urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
 // API Configuration
-export const API_BASE_URL = "http://localhost:5050";
+// export const API_BASE_URL = "http://localhost:5050";
+export const API_BASE_URL = "https://thrifty-qdg3.onrender.com";
